41a7316f12b737983bdf737db8c69db07f85ee06
YkeLit <48484963+YkeLit@users.noreply.github.com>
2023-04-16 18:11:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,12 @@ set -eux
 
 echo "::group::🔑 TEST1"
 ls && pwd
-
 echo "::endgroup::"
 
 echo "::group::🔑 TEST2"
 uname -a && who
-
 echo "::endgroup::"
 
 echo "::group::🔑 TEST3"
 cat /etc/hosts | awk "{print \$0}"
-
 echo "::endgroup::"
